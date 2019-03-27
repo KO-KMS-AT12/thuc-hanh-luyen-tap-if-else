58d17854dd95a02ece67f750712b08efcbc913d1
KO-KMS-AT12 <vtg0502@gmail.com>
2019-03-27 09:54:48 +0700

    luyen tap if else

---
+++ nhapChuoi.js
@@ -0,0 +1,9 @@
+function check() {
+    let value = prompt("What is the “official” name of JavaScript?");
+    if(value=="ECMAScript"){
+        alert("Right !");
+    }else {
+        alert("Didn’t know? ECMAScript!");
+    }
+}
+check();
